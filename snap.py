0a67c066a35a391d8cbfa325022420813697ae6d
Eesh Chilukalapalli <eeshlikhith@gmail.com>
2017-04-25 15:23:20 -0400

    black inverted

--- snap.py
+++ snap.py
@@ -435,7 +435,7 @@ class SnapRobotServer(AbstractServer):
             try:
                 image = rr.robot.camera.frame
                 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                black_image = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 20, 10)
+                black_image = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 21, 10)
                 content = cv2.imencode('.png', black_image)[1]
                 b64 = base64.encodestring(content)
                 data = 'data:image/png;base64,'+ b64
